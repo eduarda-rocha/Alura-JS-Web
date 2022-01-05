ae9dac1f46737d0947f2264936ee2a17029b09ec
Barbara <barbararocha@alunos.utfpr.edu.br>
2022-01-05 15:06:06 -0300

    Atualização

--- projeto/listaDeTarefas.js
+++ projeto/listaDeTarefas.js
@@ -3,8 +3,10 @@ const criarTarefa = (evento) => {
     evento.preventDefault()
     const input = document.querySelector('[data-form-input]')
     const valor = input.value
+    const tarefa = document.querySelector('[data-task]')
+    const conteudo = `<p class="content">${valor}</p>`
 
-    console.log(valor)
+    tarefa.innerHTML = conteudo
     input.value=""
 };
 
